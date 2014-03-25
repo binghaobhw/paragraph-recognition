c2088985fc5040644a70def674a617472afa6eca
wilfredwang <scorpio147wbh@gmail.com>
2014-03-25 22:17:32 +0800

    method using requests

---
+++ paragraph_recognition.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+import requests
+
+LTP_URL = 'http://api.ltp-cloud.com/analysis'
+API_KEY = 'u1Q1k8U6tglHca7ZZJ6qTBaq2k0QYwyXNqyE3kVu'
+FORMAT = 'json'
+PATTERN = 'all'
+param = {'api_key': API_KEY,
+         'format': FORMAT,
+         'pattern': PATTERN,
+         'text': None}
+
+
+def build_param(text):
+    param['text'] = text
+    return param
+
+
+def parse(text):
+    response = requests.get(LTP_URL, params=build_param(text))
+    return response
+
